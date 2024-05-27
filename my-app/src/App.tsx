0176b81f47914adf719daad52d48ac799b0665ee
thanh.trantruong47 <thanh.trantruong47@gmail.com>
2024-05-27 23:30:27 +0700

    Create component row

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const USERS: User[] = [
+  { name: "thanh", position: "$1", start_date: '12 Nov 2024' },
+  { name: "kim", position: "$1", start_date: '12 Nov 2024' },
+  { name: "nguyen", position: "$1", start_date: '12 Nov 2024' },
+  { name: "anh", position: "$1", start_date: '12 Nov 2024' }
+];
+
+interface User {
+  name: string;
+  position: string;
+  start_date: string;
+}
+
+const UserRow = ({ user }: { user: User }) => {
+  const name = user.name;
+  const position = user.position;
+  const startDate = user.start_date;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <tr>
+      <td>{name}</td>
+      <td>{position}</td>
+      <td>{startDate}</td>
+    </tr>
+  )
+}
+
+const UserList = ({ users }: { users: User[] }) => {
+  return (
+    <table>
+      <tbody>
+        {
+          users.map((user: User) =>
+            <UserRow key={user.name} user={user} />)
+        }
+      </tbody>
+    </table>
+  );
+}
+
+
+const App = () => {
+  return (
+    <>
+      <UserList users={USERS} />
+    </>
   );
 }
 
