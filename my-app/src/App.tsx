e017d63c4b22f6e28fdefac67320edf829337e62
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-28 15:47:18 +0700

    Fix naming start_date, add eslint for example.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 
 const USERS: User[] = [
-  { id: "1", name: "thanh", position: "$1", start_date: "12 Nov 2024" },
-  { id: "2", name: "kim", position: "$1", start_date: "12 Nov 2024" },
-  { id: "3", name: "nguyen", position: "$1", start_date: "12 Nov 2024" },
-  { id: "4", name: "anh", position: "$1", start_date: "12 Nov 2024" },
+  { id: "1", name: "thanh", position: "$1", startDate: "12 Nov 2024" },
+  { id: "2", name: "kim", position: "$1", startDate: "12 Nov 2024" },
+  { id: "3", name: "nguyen", position: "$1", startDate: "12 Nov 2024" },
+  { id: "4", name: "anh", position: "$1", startDate: "12 Nov 2024" },
 ];
 
 interface User {
   id: string;
   name: string;
   position: string;
-  start_date: string;
+  startDate: string;
 }
 
 interface UserRowProps {
@@ -23,9 +23,7 @@ interface UserListProps {
 }
 
 const UserRow = ({ user }: UserRowProps) => {
-  const name = user.name;
-  const position = user.position;
-  const startDate = user.start_date;
+  const { name, position, startDate } = user;
 
   return (
     <tr>
@@ -49,11 +47,7 @@ const UserList = ({ users }: UserListProps) => {
 };
 
 const App = () => {
-  return (
-    <>
-      <UserList users={USERS} />
-    </>
-  );
+  return <UserList users={USERS} />;
 };
 
 export default App;
