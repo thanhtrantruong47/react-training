383fba4781e300fe4c544bc6accab098d9188399
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-28 15:02:25 +0700

    Create interface for UserRow and UserList, add property id

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,28 @@
-import './App.css';
+import "./App.css";
 
 const USERS: User[] = [
-  { name: "thanh", position: "$1", start_date: '12 Nov 2024' },
-  { name: "kim", position: "$1", start_date: '12 Nov 2024' },
-  { name: "nguyen", position: "$1", start_date: '12 Nov 2024' },
-  { name: "anh", position: "$1", start_date: '12 Nov 2024' }
+  { id: "1", name: "thanh", position: "$1", start_date: "12 Nov 2024" },
+  { id: "2", name: "kim", position: "$1", start_date: "12 Nov 2024" },
+  { id: "3", name: "nguyen", position: "$1", start_date: "12 Nov 2024" },
+  { id: "4", name: "anh", position: "$1", start_date: "12 Nov 2024" },
 ];
 
 interface User {
+  id: string;
   name: string;
   position: string;
   start_date: string;
 }
 
-const UserRow = ({ user }: { user: User }) => {
+interface UserRowProps {
+  user: User;
+}
+
+interface UserListProps {
+  users: User[];
+}
+
+const UserRow = ({ user }: UserRowProps) => {
   const name = user.name;
   const position = user.position;
   const startDate = user.start_date;
@@ -24,22 +33,20 @@ const UserRow = ({ user }: { user: User }) => {
       <td>{position}</td>
       <td>{startDate}</td>
     </tr>
-  )
-}
+  );
+};
 
-const UserList = ({ users }: { users: User[] }) => {
+const UserList = ({ users }: UserListProps) => {
   return (
     <table>
       <tbody>
-        {
-          users.map((user: User) =>
-            <UserRow key={user.name} user={user} />)
-        }
+        {users.map((user: User) => (
+          <UserRow key={user.id} user={user} />
+        ))}
       </tbody>
     </table>
   );
-}
-
+};
 
 const App = () => {
   return (
@@ -47,6 +54,6 @@ const App = () => {
       <UserList users={USERS} />
     </>
   );
-}
+};
 
 export default App;
