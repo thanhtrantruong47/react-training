317bd17962bad34262f12b2291c264a672a25d5e
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-29 10:04:52 +0700

    Fix file eslint config

--- examples/my-first-react-app/eslint.config.mjs
+++ examples/my-first-react-app/eslint.config.mjs
@@ -1,13 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+// @ts-check
 
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-];
+);
