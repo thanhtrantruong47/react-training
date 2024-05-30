798f0643d02a18dd2d90c2ce85eba42627167835
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-05-30 14:56:15 +0700

    Add package react to project javascript.

---
+++ examples/practice-javascript/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,5 @@
+function NavigationBar() {
+  return <h1>Hello from React!</h1>;
+}
+
+export default NavigationBar;
--- examples/practice-javascript/src/scripts/index.js
+++ examples/practice-javascript/src/scripts/index.js
@@ -1,14 +1,6 @@
-import UserController from "./controllers/userController";
-import UserView from "./views/userView";
-import UserService from "./services/userService";
+import { createRoot } from "react-dom/client";
+import NavigationBar from "../components/NavigationBar/NavigationBar";
 
-window.addEventListener("load", () => {
-  // call function for page login
-  const managerUser = new UserController(new UserView(), new UserService());
-  managerUser.userLogin();
-  managerUser.displayUser();
-  managerUser.userDelete();
-  managerUser.displayFormEdit();
-  managerUser.editUser();
-  managerUser.userSignUp();
-});
+const domNode = document.getElementById("navigation");
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
