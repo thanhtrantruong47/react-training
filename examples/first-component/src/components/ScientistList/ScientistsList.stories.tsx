8cb4b1430c6a73c5bc587ace2eb3ead9b8d28ee3
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-06-03 16:09:12 +0700

    Apply storybook for example first component.

---
+++ examples/first-component/src/components/ScientistList/ScientistsList.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ScientistList from "./index";
+const meta: Meta<typeof ScientistList> = {
+  component: ScientistList,
+};
+
+export default meta;
+type Story = StoryObj<typeof ScientistList>;
+const scientist = [
+  {
+    imageId:
+      "https://khunganhonline.com/images/frame/frame_icon/khung-anh-hoa-hinh5aec7498e4003.jpg",
+    name: "Marie Curie",
+    profession: "Physicist & Chemist",
+    awards: "Nobel Prize in Physics, Nobel Prize in Chemistry",
+    discoveries: "Radioactivity, Polonium, Radium",
+    imageSize: 1400,
+  },
+  {
+    imageId:
+      "https://khunganhonline.com/images/frame/frame_icon/khung-anh-hoa-hinh5aec7498e4003.jpg",
+    name: "Albert Einstein",
+    profession: "Theoretical Physicist",
+    awards: "Nobel Prize in Physics",
+    discoveries: "Theory of Relativity, Photoelectric Effect",
+    imageSize: 800,
+  },
+  {
+    imageId:
+      "https://khunganhonline.com/images/frame/frame_icon/khung-anh-hoa-hinh5aec7498e4003.jpg",
+    name: "Ada Lovelace",
+    profession: "Mathematician & Writer",
+    awards: "N/A",
+    discoveries: "First Computer Programmer",
+    imageSize: 720,
+  },
+];
+
+export const ScientistsList: Story = {
+  args: {
+    scientists: scientist,
+  },
+};
