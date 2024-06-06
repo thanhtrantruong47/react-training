d76294123fe7f1a791805ed83c4a701292fbd3f8
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-06-06 11:27:17 +0700

    Create component Logo.

---
+++ examples/practice-typescript/src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import './logo.scss';
+
+const Logo = () => {
+  return (
+    <div className="logo-group">
+      <a className="icon-logo" href="javascript:void(0)"></a>
+      <h1>Company name</h1>
+    </div>
+  );
+};
+
+export default Logo;
--- examples/practice-typescript/src/scripts/index.tsx
+++ examples/practice-typescript/src/scripts/index.tsx
@@ -1,17 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import React from 'react';
-
-const NavigationBar = () => {
-  return <h1>Hello from React!</h1>;
-};
+import Logo from 'components/Logo/Logo';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
-  <>
-    <StrictMode>
-      <NavigationBar />
-    </StrictMode>
-  </>
+  <StrictMode>
+    <Logo />
+  </StrictMode>
 );
