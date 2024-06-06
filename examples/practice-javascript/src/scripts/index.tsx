d1111756a5e984e904dad7fb0ccc22c3879e4266
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-06-06 08:42:18 +0700

    Config practice.

--- examples/practice-javascript/src/scripts/index.tsx
+++ examples/practice-javascript/src/scripts/index.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
-import Logo from "../components/Logo/Logo";
 import { StrictMode } from "react";
 
 const domNode = document.getElementById("root") as HTMLElement;
 const root = createRoot(domNode);
 root.render(
   <>
-    <StrictMode>
-      <Logo />
-    </StrictMode>
+    <StrictMode></StrictMode>
   </>
 );
