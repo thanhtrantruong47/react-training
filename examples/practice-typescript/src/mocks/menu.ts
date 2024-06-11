126199e12185632442b6322f9f1c5d692e8e2cb4
thanhtrantruong47 <155411536+thanhtrantruong47@users.noreply.github.com>
2024-06-11 10:51:12 +0700

    Implement component Sidebar (#19)
    * Implement component SideBar.
    
    * Fix warning.
    
    * Delete code duplicate.
    
    * Fix comments, create type menu.
    
    * Rename file and fix comments.
    
    * Fix import variable in component.

---
+++ examples/practice-typescript/src/mocks/menu.ts
@@ -0,0 +1,14 @@
+export const MenuItems = [
+  {
+    id: '1',
+    text: 'User management',
+    router: '/component.html',
+    isActive: true,
+  },
+  {
+    id: '2',
+    text: 'Product management',
+    router: '/component.html',
+    isActive: false,
+  },
+];
