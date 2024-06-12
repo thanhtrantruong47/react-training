58c83191a006be74fa5bbf7fb70cd527fa3a22ca
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-06-12 10:03:21 +0700

    Fix comment use inport image and shorthand code.

--- examples/practice-typescript/src/components/TableController/index.tsx
+++ examples/practice-typescript/src/components/TableController/index.tsx
@@ -1,19 +1,18 @@
 import Button from 'components/Button';
 import InputField from 'components/Input';
-import './index.scss'
+import searchIcon from '../../assets/images/managers/icon-search.png';
+import './index.scss';
 
-const TableController = () => {
-  return (
-    <div className='table-control'>
-      <InputField
-        icon={require('../../assets/images/managers/icon-search.png')}
-        typeInput="search"
-        description="Search All"
-        size="17"
-      />
-      <Button text="Add new" styleClass="btn-add" />
-    </div>
-  );
-};
+const TableController = () => (
+  <div className="table-control">
+    <InputField
+      icon={searchIcon}
+      typeInput="search"
+      description="Search All"
+      size="17"
+    />
+    <Button text="Add new" styleClass="btn-add" />
+  </div>
+);
 
 export default TableController;
