0c32374a086a573bba05f60cade9d9e196342162
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-05 14:27:05 +0700

    fix: duplicate code, style and code not use

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './routers';
 
-function App() {
+const App = () => {
   return (
     <>
       <RouterProvider router={router} />
     </>
   );
-}
+};
 
 export default App;
