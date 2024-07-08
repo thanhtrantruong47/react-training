15e58b38175c0703293919625e1ee8c9bd90260a
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-08 09:43:23 +0700

    feat: create type, service, test api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import ProductService from '../../services';
+import { Product } from '../../types/product';
+
 const Home = () => {
-  return <></>;
+  const [product, setProduct] = useState<Product[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const productService = new ProductService();
+        const fetchedProduct = await productService.getProductByCategory('1');
+        setProduct(fetchedProduct);
+      } catch (err) {
+        setError(err.message || 'Failed to fetch product');
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return <>{console.log(product)}</>;
 };
 
 export default Home;
---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  title: string;
+  price: number;
+  image: string[];
+  color: string[];
+  size: string[];
+  description: string;
+  stock: number;
+  rate: number;
+  numberRating: number;
+  id: number;
+}
