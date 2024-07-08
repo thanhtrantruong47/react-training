d6cbd2059c828570df5cd3cafe58764869f159c7
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-08 14:35:12 +0700

    fix: delete service

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,5 @@
-import { useEffect, useState } from 'react';
-import ProductService from '../../services';
-import { Product } from '../../types/product';
-
 const Home = () => {
-  const [product, setProduct] = useState<Product[] | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const productService = new ProductService();
-        const fetchedProduct = await productService.getProductByCategory('1');
-        setProduct(fetchedProduct);
-      } catch (err) {
-        setError(err.message || 'Failed to fetch product');
-      }
-    };
-
-    fetchProduct();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  return <>{console.log(product)}</>;
+  return <></>;
 };
 
 export default Home;
