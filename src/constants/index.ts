175ae00edf482f62a950af953377e146a199e8f1
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-08 16:26:42 +0700

    feat: create services, assets

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+// constants.ts
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const PRODUCTS = 'products';
---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  images: string[];
+  colors: string[];
+  sizes: string[];
+  description: string;
+  stock: number;
+  rate: number;
+  numberRating: number;
+}
