565439e85fe629b5b78a55ad3b96777d0802f133
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-09 22:20:53 +0700

    fix: onclick not define

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routers';
-import NavBarItem from './components/NavBarItem/NavBarItem';
 
 const App = () => {
   return (
     <>
-      <NavBarItem label="Home" destination="/" isActive={true} />
-      <NavBarItem label="Home" destination="/" isActive={false} />
       <RouterProvider router={router} />
     </>
   );
