46d07392cdb27ec6f1c310b44c535f374e0df49e
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-10 16:53:50 +0700

    feat: create component logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,18 @@
+// Logo.tsx
+import companyLogo from '../../assets/images/logo.svg';
+import styles from './logo.module.css';
+
 const Logo = () => {
-  return <></>;
+  return (
+    <div>
+      <svg height="34" width="160">
+        <image width="35" height="34" href={companyLogo} />
+        <text className={styles.logo} x="44" y="50%" dominantBaseline="middle">
+          mangcoding Store
+        </text>
+      </svg>
+    </div>
+  );
 };
 
 export default Logo;
