0529be9ff647e4be2ccca703ecdd8c9c915ece35
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-10 20:14:44 +0700

    feat: create navbar story bôk

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routers';
 import { useEffect } from 'react';
 import ProductAPIService from './services/ProductAPIService';
-import NavBar from './components/NavBar/NavBar';
-import { NavItem } from './types/navBar';
 
 const App = () => {
   // example start
@@ -27,17 +25,9 @@ const App = () => {
 
   return (
     <>
-      <NavBar items={navItems} />
       <RouterProvider router={router} />
     </>
   );
 };
-
-const navItems: NavItem[] = [
-  { label: 'Home', destination: '/', isActive: true },
-  { label: 'Shop', destination: 'javascript:void(0)', isActive: false },
-  { label: 'About Us', destination: 'javascript:void(0)', isActive: false },
-  { label: 'Contact', destination: 'javascript:void(0)', isActive: false },
-];
 
 export default App;
--- src/components/NavBar/NavBar.stories.tsx
+++ src/components/NavBar/NavBar.stories.tsx
@@ -1,13 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import NavBar from './NavBar';
+import { NavItem } from '../../types/navBar';
 
 export default {
   component: NavBar,
   title: 'Components/NavBar',
 } as Meta;
 
+const navItems: NavItem[] = [
+  { label: 'Home', destination: 'javascript:void(0)', isActive: true },
+  { label: 'Shop', destination: 'javascript:void(0)', isActive: false },
+  { label: 'About Us', destination: 'javascript:void(0)', isActive: false },
+  { label: 'Contact', destination: 'javascript:void(0)', isActive: false },
+];
+
 type Story = StoryObj<typeof NavBar>;
 
 export const Default: Story = {
-  args: {},
+  args: { items: navItems },
 };
