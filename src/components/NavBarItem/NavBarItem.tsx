4e8c79c5369176109180cd860dbd88e76367c913
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-11 15:31:08 +0700

    feat: create folder modules

--- src/components/NavBarItem/NavBarItem.tsx
+++ src/components/NavBarItem/NavBarItem.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import utilStyles from '../../styles/utils.module.css';
+import utilStyles from '../../styles/modules/utils.module.css';
 import styles from './navBarItem.module.css';
 
 interface NavBarItemProps {
