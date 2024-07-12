b268cdeea98a9ba204776b976eeff54cc1c9a3e9
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-12 16:35:25 +0700

    feat: extend  input html

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -15,6 +15,6 @@ export const Default: Story = {
     placeholder: 'Search',
     inputType: 'input',
     onChange: fn(),
-    children: <SearchIcon />,
+    endIcon: <SearchIcon />,
   },
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,22 @@
-import { ReactNode } from 'react';
+import { InputHTMLAttributes, ReactNode } from 'react';
 import styles from './input.module.css';
 import utils from '../../styles/modules/utils.module.css';
 import EndIcon from '../Icon/EndIcon';
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   inputType: string;
   onChange: () => void;
-  children?: ReactNode;
+  endIcon?: ReactNode;
 }
 
-const Input = ({ placeholder, inputType, onChange, children }: InputProps) => (
+const Input = ({
+  placeholder,
+  inputType,
+  onChange,
+  endIcon,
+  ...rest
+}: InputProps) => (
   <div className={utils.relative}>
     <input
       type={inputType}
@@ -18,8 +24,9 @@ const Input = ({ placeholder, inputType, onChange, children }: InputProps) => (
       onChange={onChange}
       className={styles.searchInput}
       maxLength={20}
+      {...rest}
     />
-    <EndIcon>{children}</EndIcon>
+    <EndIcon>{endIcon}</EndIcon>
   </div>
 );
 
