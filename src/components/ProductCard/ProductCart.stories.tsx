4c82bcdacd7081dba727a15731e203d05f590680
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-15 16:33:03 +0700

    fix: height image

--- src/components/ProductCard/ProductCart.stories.tsx
+++ src/components/ProductCard/ProductCart.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ProductCart from './ProductCart';
-import productImage from '../../assets/images/banner.jpg';
+import productImage from '../../assets/images/delete.svg';
 
 export default {
   component: ProductCart,
