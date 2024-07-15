90880de2918ca9e52b1924107a10f05b1fcca539
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-16 00:48:57 +0700

    feat: component Radio

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,13 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
+
 import Radio from './Radio';
 
-export default {
+const meta: Meta<typeof Radio> = {
   component: Radio,
-  title: 'Components/Radio',
-} as Meta;
+};
 
+const colorOptions = ['red', 'green', 'blue'];
+const sizeOptions = ['S', 'M', 'L'];
+
+export default meta;
 type Story = StoryObj<typeof Radio>;
 
-export const Default: Story = {
-  args: {},
+export const InputRadioColor: Story = {
+  args: {
+    variant: 'color',
+    options: colorOptions,
+  },
+};
+
+export const InputRadioSize: Story = {
+  args: {
+    variant: 'sizes',
+    options: sizeOptions,
+  },
 };
--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,15 +1,89 @@
-interface RadioOption {
-  variable: string;
-  value: number;
-}
+import React from 'react';
+import styles from './radio.module.css';
+import utils from '../../styles/modules/utils.module.css';
 
-interface RadioProps {
-  options: RadioOption;
-  onChange: () => void;
-}
+const ColorOption = ({
+  color,
+  isFirstOption,
+}: {
+  color: string;
+  isFirstOption: boolean;
+}) => (
+  <>
+    <input
+      type="radio"
+      name="color"
+      id={color}
+      className={styles.colorRadio}
+      defaultChecked={isFirstOption}
+    />
+    <label htmlFor={color} className={styles.colorOption}>
+      <div
+        className={styles.colorCircle}
+        style={{ backgroundColor: color } as React.CSSProperties}
+      />
+    </label>
+  </>
+);
 
-const Radio = (_: RadioProps) => {
-  return <></>;
+const SizeOption = ({
+  size,
+  isFirstOption,
+}: {
+  size: string;
+  isFirstOption: boolean;
+}) => (
+  <>
+    <input
+      type="radio"
+      name="size"
+      id={size}
+      className={styles.sizeRadio}
+      defaultChecked={isFirstOption}
+    />
+    <label
+      htmlFor={size}
+      className={`${styles.sizeOption} ${utils.flexCenter}`}
+    >
+      {size}
+    </label>
+  </>
+);
+
+const ListColor = ({ colors }: { colors: string[] }) => (
+  <div>
+    <p>Colors</p>
+    <div className={styles.listColor}>
+      {colors.map((option, index) => (
+        <ColorOption color={option} key={option} isFirstOption={index === 0} />
+      ))}
+    </div>
+  </div>
+);
+
+const ListSize = ({ sizes }: { sizes: string[] }) => (
+  <div>
+    <p>Size</p>
+    <div className={styles.listSize}>
+      {sizes.map((option, index) => (
+        <SizeOption size={option} key={option} isFirstOption={index === 0} />
+      ))}
+    </div>
+  </div>
+);
+
+const Radio = ({
+  variant,
+  options,
+}: {
+  variant: string;
+  options: string[];
+}) => {
+  return variant === 'color' ? (
+    <ListColor colors={options} />
+  ) : (
+    <ListSize sizes={options} />
+  );
 };
 
 export default Radio;
