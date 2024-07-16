6a7ac045aa2a2cc7576270816c5b3cd47829fa20
thanhtrantruong47 <155411536+thanhtrantruong47@users.noreply.github.com>
2024-07-16 16:37:19 +0700

    Implement component  cart product  (#54)
    * feat: component card product
    
    * fix: renaming component
    
    * feat: handle rating
    
    * fix: add class rating
    
    * fix: break  layout
    
    * fix: height image
    
    * feat: split function
    
    * feat: user title and index for key
    
    * fix: code style
    
    * fix: add break line
    
    * fix: create seclected in product cart
    
    * fix: delete code not use
    
    * fix: delete onChange in Icon
    
    * fix: use button for bookmark

--- src/components/ProductCard/ProductCart.tsx
+++ src/components/ProductCard/ProductCart.tsx
@@ -1,14 +1,57 @@
+import utils from '../../styles/modules/utils.module.css';
+import StarIcon from '../Icon/StarIcon';
+import styles from './productCart.module.css';
+import { formatNumberCompact } from '../../utils/formatNumberCompact';
+import BookMarkIcon from '../Icon/BookMarkIcon';
+import LikedIcon from '../Icon/LikedIcon';
+
 interface ProductCardProps {
   image: string;
   title: string;
   price: number;
   description: string;
   rating: number;
-  isActive: boolean;
+  ratingNumbers: number;
+  onChange?: () => void;
 }
 
-const ProductCard = (_: ProductCardProps) => {
-  return <></>;
-};
+const ProductCard = ({
+  image,
+  title,
+  price,
+  description,
+  rating,
+  ratingNumbers,
+  onChange,
+}: ProductCardProps) => (
+  <div className={styles.product}>
+    <div className={`${styles.imageGroup} ${utils.relative}`}>
+      <img className={styles.productImage} src={image} alt={title} />
+      <div className={styles.bookMark}>
+        <button aria-label="toggle bookmark" onChange={onChange}>
+          <BookMarkIcon />
+        </button>
+        <button aria-label="toggle liked" onChange={onChange}>
+          <LikedIcon />
+        </button>
+      </div>
+    </div>
+    <div className={styles.group}>
+      <div className={`${styles.nameGroup} ${utils.flexCenter}`}>
+        <p>{title}</p>
+        <span>{price}$</span>
+      </div>
+      <p className={styles.desc}>{description}</p>
+      <div className={`${styles.ratingGroup} ${utils.flexCenter}`}>
+        <div className={`${styles.numberRating} ${utils.flexCenter}`}>
+          {Array.from({ length: ratingNumbers }).map((_, index) => (
+            <StarIcon key={`${title}_${index}`} />
+          ))}
+        </div>
+        <p className={styles.ratingGroup}>{formatNumberCompact(rating)}</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default ProductCard;
