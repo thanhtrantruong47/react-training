e2aab31aac641aa1c4ab80c84445ad275bd1653b
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-17 10:06:45 +0700

    fix: comment naming product, id

--- src/components/ProductCard/ProductCart.tsx
+++ src/components/ProductCard/ProductCart.tsx
@@ -4,8 +4,10 @@ import styles from './productCart.module.css';
 import { formatNumberCompact } from '../../utils/formatNumberCompact';
 import BookMarkIcon from '../Icon/BookMarkIcon';
 import LikedIcon from '../Icon/LikedIcon';
+import { Link } from 'react-router-dom';
 
 interface ProductCardProps {
+  id: number;
   image: string;
   title: string;
   price: number;
@@ -16,6 +18,7 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({
+  id,
   image,
   title,
   price,
@@ -24,34 +27,36 @@ const ProductCard = ({
   rate,
   onChange,
 }: ProductCardProps) => (
-  <div className={styles.product}>
-    <div className={`${styles.imageGroup} ${utils.relative}`}>
-      <img className={styles.productImage} src={image} alt={title} />
-      <div className={styles.bookMark}>
-        <button aria-label="toggle bookmark" onChange={onChange}>
-          <BookMarkIcon />
-        </button>
-        <button aria-label="toggle liked" onChange={onChange}>
-          <LikedIcon />
-        </button>
-      </div>
-    </div>
-    <div className={styles.group}>
-      <div className={`${styles.nameGroup} ${utils.flexCenter}`}>
-        <p>{title}</p>
-        <span>{price}$</span>
-      </div>
-      <p className={styles.desc}>{description}</p>
-      <div className={`${styles.ratingGroup} ${utils.flexCenter}`}>
-        <div className={`${styles.numberRating} ${utils.flexCenter}`}>
-          {Array.from({ length: numberRating }).map((_, index) => (
-            <StarIcon key={`${title}_${index}`} />
-          ))}
+  <Link className={utils.link} to={`/detail/${id}`}>
+    <div className={styles.product}>
+      <div className={`${styles.imageGroup} ${utils.relative}`}>
+        <img className={styles.productImage} src={image} alt={title} />
+        <div className={styles.bookMark}>
+          <button aria-label="toggle bookmark" onChange={onChange}>
+            <BookMarkIcon />
+          </button>
+          <button aria-label="toggle liked" onChange={onChange}>
+            <LikedIcon />
+          </button>
         </div>
-        <p className={styles.ratingGroup}>({formatNumberCompact(rate)})</p>
+      </div>
+      <div className={styles.group}>
+        <div className={`${styles.nameGroup} ${utils.flexCenter}`}>
+          <p>{title}</p>
+          <span>{price}$</span>
+        </div>
+        <p className={styles.desc}>{description}</p>
+        <div className={`${styles.ratingGroup} ${utils.flexCenter}`}>
+          <div className={`${styles.numberRating} ${utils.flexCenter}`}>
+            {Array.from({ length: numberRating }).map((_, index) => (
+              <StarIcon key={`${title}_${index}`} />
+            ))}
+          </div>
+          <p className={styles.ratingGroup}>({formatNumberCompact(rate)})</p>
+        </div>
       </div>
     </div>
-  </div>
+  </Link>
 );
 
 export default ProductCard;
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,5 @@
-import { Link } from 'react-router-dom';
 import { Product } from '../../types/product';
 import ProductCard from '../ProductCard/ProductCart';
-import utils from '../../styles/modules/utils.module.css';
 import styles from './productList.module.css';
 
 interface ProductListProps {
@@ -13,16 +11,16 @@ const ProductList = ({ products }: ProductListProps) => (
   <div className={styles.container}>
     {products.map(
       ({ id, title, price, description, rate, numberRating, images }) => (
-        <Link className={utils.link} to={`/detail/${id}`} key={id}>
-          <ProductCard
-            title={title}
-            description={description}
-            image={images[0]}
-            numberRating={numberRating}
-            price={price}
-            rate={rate ? rate : 0}
-          />
-        </Link>
+        <ProductCard
+          key={id}
+          id={id}
+          title={title}
+          description={description || ''}
+          image={images ? images[0] : ''}
+          numberRating={numberRating || 0}
+          price={price}
+          rate={rate || 0}
+        />
       )
     )}
   </div>
