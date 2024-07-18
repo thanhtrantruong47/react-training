9e1053297e1d09b011f3088b65df6b963ea13966
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-18 14:53:32 +0700

    feat: add icon delete

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,40 +1,47 @@
-import { CartIcon } from '../Icon/CartIcon';
 import styles from './cartItem.module.css';
 import utils from '../../styles/modules/utils.module.css';
 import NumberInput from '../NumberInput/NumberInput';
+import DeleteIcon from '../Icon/DeleteIcon';
+import Button from '../Button/Button';
 
 interface CartItemProps {
   image: string;
   title: string;
   price: number;
+  color: string;
   quantity: number;
   stock: number;
 }
 
-const CartItem = (_: CartItemProps) => (
+const CartItem = ({
+  image,
+  title,
+  price,
+  color,
+  quantity,
+  stock,
+}: CartItemProps) => (
   <div className={styles.container}>
     <div className={`${utils.flexCenter} ${styles.group}`}>
-      <img
-        className={styles.image}
-        src="https://i.pinimg.com/236x/74/20/58/742058099ee4749d2da5d820396870eb.jpg"
-        alt="sadasd"
-      />
+      <img className={styles.image} src={image} alt={image} />
       <div className={styles.option}>
-        <p className={styles.title}>Name</p>
-        <p className={styles.desc}>Color: White</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.desc}>
+          Color: {color.charAt(0).toUpperCase() + color.slice(1)}
+        </p>
       </div>
       <div className={styles.option}>
-        <p className={styles.title}>$40 USD</p>
+        <p className={styles.title}>${price} USD</p>
         <p className={styles.desc}>Price</p>
       </div>
       <div className={styles.option}>
-        <p className={styles.title}>Stock</p>
-        <p className={styles.desc}>In stock</p>
+        <p className={styles.title}>{stock}</p>
+        <p className={styles.desc}>In Stock</p>
       </div>
-      <CartIcon />
+      <Button className={styles.button} icon={DeleteIcon} />
     </div>
     <div className={`${utils.flexCenter} ${styles.quantity}`}>
-      <NumberInput defaultValue={1} />
+      <NumberInput defaultValue={quantity} />
     </div>
   </div>
 );
