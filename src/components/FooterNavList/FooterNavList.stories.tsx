d260d57254e3901ca9574801e8fd56bb408cdfb6
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-19 11:56:24 +0700

    feat: create component FooterNavList

--- src/components/FooterNavList/FooterNavList.stories.tsx
+++ src/components/FooterNavList/FooterNavList.stories.tsx
@@ -6,8 +6,16 @@ export default {
   title: 'Components/FooterNavList',
 } as Meta;
 
+const navItems = [
+  { label: 'Home', path: '/' },
+  { label: 'Product', path: '/product' },
+  { label: 'Categories', path: '/categories' },
+  { label: 'Shop', path: '/shop' },
+  { label: 'Log in', path: '/login' },
+];
+
 type Story = StoryObj<typeof FooterNavList>;
 
 export const Default: Story = {
-  args: {},
+  args: { items: navItems },
 };
--- src/components/FooterNavList/FooterNavList.tsx
+++ src/components/FooterNavList/FooterNavList.tsx
@@ -1,9 +1,32 @@
-interface FooterNavListProps {
-  items: string[];
+import { Link } from 'react-router-dom';
+import styles from './footerNavList.module.css';
+
+interface NavItem {
+  label: string;
+  path: string;
 }
 
-const FooterNavList = (_: FooterNavListProps) => {
-  return <></>;
+interface FooterNavListProps {
+  items: NavItem[];
+}
+
+const FooterNavList = ({ items }: FooterNavListProps) => {
+  const [firstItem, ...restItems] = items;
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>{firstItem.label}</p>
+      <ul className={styles.item}>
+        {restItems.map(({ label, path }) => (
+          <li key={label}>
+            <Link className={styles.link} to={path}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default FooterNavList;
