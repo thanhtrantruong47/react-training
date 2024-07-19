224d30a40dffb6c7ce6d766a643dc4ea94b64ddf
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-19 11:04:08 +0700

    feat: storybook ProductDeatil

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,23 +1,5 @@
-import ProductDetail from '../../components/ProductDetail/ProductDetail';
-import image1 from '../../assets/images/detail-small.jpg';
-import image2 from '../../assets/images/small-t-shirt.jpg';
-
-const productDetailData = {
-  id: 1,
-  images: [image1, image2, image1],
-  title: 'Smart T-Shirt',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum mi sed sem finibus ullamcorper.',
-  price: 40,
-  colors: ['Black', 'Blue', 'Green'],
-  sizes: ['S', 'M', 'L'],
-  stock: 50,
-  rate: 4,
-  numberRating: 5555,
-};
-
 const Details = () => {
-  return <>{<ProductDetail {...productDetailData} />}</>;
+  return <></>;
 };
 
 export default Details;
