1094363540aa237a08dd72cc97a2e772978a8023
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-21 22:24:30 +0700

    feat: hero section

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,24 @@
+import { LIST_IMAGES_CAROUSEL } from '../../constants';
+import Button from '../Button/Button';
+import HeroCarousel from '../HeroCarousel/HeroCarousel';
+import styles from './heroSection.module.css';
+
 const HeroSection = () => {
-  return <></>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.content}>
+        <p className={styles.title}>Get the Latest Dress Models From Us</p>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget gravida leo, nec iaculis diam. Nam bibendum
+          mi sed sem finibus ullamcorper
+        </p>
+        <div className={styles.buttonContainer}>
+          <Button classStyle={styles.button} content="Shop now" />
+        </div>
+      </div>
+      <HeroCarousel images={LIST_IMAGES_CAROUSEL} />
+    </div>
+  );
 };
 
 export default HeroSection;
