a46e00418ab24eabfacfbba53acf0047af761d2c
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-22 10:22:46 +0700

    feat: add width

--- src/components/CartList/CartList.tsx
+++ src/components/CartList/CartList.tsx
@@ -9,15 +9,7 @@ interface CartListProps {
 const CartList = ({ cartItems }: CartListProps) => (
   <div className={styles.container}>
     {cartItems.map(({ id, image, title, price, color, quantity, stock }) => (
-      <CartItem
-        key={id}
-        color={color}
-        image={image}
-        price={price}
-        quantity={quantity}
-        stock={stock}
-        title={title}
-      />
+      <CartItem key={id} color={color} image={image} price={price} quantity={quantity} stock={stock} title={title} />
     ))}
   </div>
 );
