b4bade2fa90341901e16991b6286c34ed4607b12
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-24 09:38:11 +0700

    fix: add url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { NavItem } from '../types/navBar';
 
 // constants.ts
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const BASE_URL = 'https://6684bd0356e7503d1ae0ff6f.mockapi.io';
 export const PRODUCTS = 'products';
 
 export const NAV_BAR_ITEMS: NavItem[] = [
