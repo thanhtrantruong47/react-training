1303abff2142564a044e5f39ca7fdba58c9ca5c2
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-07-29 08:38:38 +0700

    fix: code style

--- src/components/Icon/SearchIcon.tsx
+++ src/components/Icon/SearchIcon.tsx
@@ -1,12 +1,6 @@
 export const SearchIcon = () => (
   <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
-    <circle
-      cx="7.875"
-      cy="7.875"
-      r="5.0625"
-      stroke="currentColor"
-      strokeWidth="1.125"
-    />
+    <circle cx="7.875" cy="7.875" r="5.0625" stroke="currentColor" strokeWidth="1.125" />
     <rect
       x="11.8687"
       y="11.1616"
