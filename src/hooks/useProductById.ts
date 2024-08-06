34e2ae782db4b2b5887ccd4cbef0aef8100e576e
thanhtrantruong47 <155411536+thanhtrantruong47@users.noreply.github.com>
2024-08-06 11:00:22 +0700

    Handle error detail client error responses (#90)
    * feat: handle error when error 404
    
    * fix: break line, enum status
    
    * fix: add break line in hook
    
    * fix: delete redundant code
    
    * fix: use constants
    
    * fix: refactor detail

--- src/hooks/useProductById.ts
+++ src/hooks/useProductById.ts
@@ -2,37 +2,52 @@ import { useState, useEffect } from 'react';
 import { Product as ProductType } from '@/types/product';
 import { ProductAPIService } from '@/services/ProductAPIService';
 import { PRODUCTS_MOCK as mockProducts } from '@/mocks/products';
+import { STATUS } from '@/constants';
 
 const useMockData = import.meta.env.USE_MOCK_FOR_API_FAIL === 'true';
 
 interface UseProductByIdResult {
   product: ProductType | null;
   isLoading: boolean;
+  status?: number; // Optional status code
 }
 
-const fetchProductById = async (productId: string) => {
+const fetchProductById = async (productId: string, useMockData: boolean) => {
   if (!productId) {
-    return null;
+    return { data: null, status: STATUS.NOT_FOUND };
   }
 
-  const response = await ProductAPIService.getById(productId);
+  if (useMockData) {
+    const mockProduct = mockProducts.find(product => product.id.toString() === productId) || null;
 
-  if (response.isSuccess && response.data !== undefined && !useMockData) {
-    return response.data;
+    return { data: mockProduct, status: 200 }; // Assuming mock data is always successful
   }
 
-  return mockProducts.find(product => product.id.toString() === productId) || null;
+  try {
+    const response = await ProductAPIService.getById(productId);
+    if (response.isSuccess && response.data !== undefined) {
+      return { data: response.data, status: response.status };
+    }
+
+    return { data: null, status: response.status };
+  } catch (error) {
+    console.error('Failed to fetch product:', error);
+
+    return { data: null, status: 500 }; // Assuming a 500 status code for errors
+  }
 };
 
 export const useProductById = (id: string): UseProductByIdResult => {
   const [product, setProduct] = useState<ProductType | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [status, setStatus] = useState<number>();
 
   useEffect(() => {
     const fetchProductData = async () => {
       setIsLoading(true);
-      const fetchedProduct = await fetchProductById(id);
-      setProduct(fetchedProduct);
+      const { data, status } = await fetchProductById(id, useMockData);
+      setProduct(data);
+      setStatus(status);
       setIsLoading(false);
     };
 
@@ -41,5 +56,5 @@ export const useProductById = (id: string): UseProductByIdResult => {
     }
   }, [id]);
 
-  return { product, isLoading };
+  return { product, isLoading, status };
 };
--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -2,48 +2,51 @@ import { useParams } from 'react-router-dom';
 import styles from './productDetail.module.css';
 import MainLayout from '@/layouts';
 import { default as styleUtils } from '@/styles/modules/utils.module.css';
-import { useProducts } from '@/hooks/'; // Import the custom hook
+import { useProducts } from '@/hooks/';
 import { useProductById } from '@/hooks/useProductById';
 import { Product, Loading, ProductList } from '@/components';
+import { STATUS } from '@/constants';
 
 const ProductDetail = () => {
   const { id } = useParams<{ id: string }>();
 
+  // Use the useProductById hook for the product and status
+  const { product, isLoading: isProductLoading, status } = useProductById(id || '');
+
   // Use the useProducts hook for related products
-  const { product, isLoading: isProductLoading } = useProductById(id || '');
-  const { products, isLoading: isRelatedProductsLoading, hasMore, onLoadMore } = useProducts(product?.category || '');
+  const { products, hasMore, onLoadMore } = useProducts(product?.category || '');
+
+  const isNotFound = status === STATUS.NOT_FOUND || !product;
 
   return (
     <MainLayout>
-      <div>
-        <div className={styles.product}>
-          {isProductLoading ? (
-            <Loading classStyle={styleUtils.loading} />
-          ) : (
-            product && (
-              <Product
-                colors={product.colors || []}
-                description={product.description || ''}
-                images={product.images || []}
-                numberRating={product.numberRating || 0}
-                price={product.price || 0}
-                rate={product.rate || 0}
-                sizes={product.sizes || []}
-                stock={product.stock || 0}
-                title={product.title || ''}
-              />
-            )
-          )}
+      {isProductLoading ? (
+        <Loading classStyle={`${styleUtils.loading} ${styles.loading}`} />
+      ) : isNotFound ? (
+        <div className={styleUtils.container}>
+          <p className={styles.error}>Product not found</p>
         </div>
-        <div className={styles.list}>
-          <p className={styles.title}>Same Product</p>
-          {isRelatedProductsLoading && products.length === 0 ? (
-            <Loading classStyle={styleUtils.loading} />
-          ) : (
-            <ProductList products={products} onClick={onLoadMore} hasMore={hasMore} />
-          )}
+      ) : (
+        <div>
+          <div className={styles.product}>
+            <Product
+              colors={product.colors || []}
+              description={product.description || ''}
+              images={product.images || []}
+              numberRating={product.numberRating || 0}
+              price={product.price || 0}
+              rate={product.rate || 0}
+              sizes={product.sizes || []}
+              stock={product.stock || 0}
+              title={product.title || ''}
+            />
+            <div className={styles.list}>
+              <p className={styles.title}>Same Product</p>
+              <ProductList products={products} onClick={onLoadMore} hasMore={hasMore} />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </MainLayout>
   );
 };
